625efdbfe309d380dd725e74fb1f2314b63b2f3f
Anh Nguyen <nguyenhoanganh219@gmail.com>
2023-12-13 02:21:41 +0200

    Fine-tune dialog with different size. Add 7 dialog examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,120 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
 import { AppHeaderComponent } from './common/layout/app-header.component';
 import { NordeaDialogComponent } from './nordea-dialog/nordea-dialog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AppHeaderComponent, NordeaDialogComponent],
+  imports: [
+    HttpClientModule,
+    CommonModule,
+    NzButtonModule,
+    NzCheckboxModule,
+    NzFormModule,
+    NzInputModule,
+    RouterOutlet,
+    AppHeaderComponent,
+    NordeaDialogComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'nordea-angular-assignment';
-
-  isDialog1Open = false;
-  dialog1Title = 'Dialog 1 Title';
-
-  isDialog2Open = false;
-  dialog2Title = 'Dialog 2 Title';
+  // Dialog 1
+  dialog1Visible = false;
+  dialog1Title = 'Simple dialog';
 
   openDialog1() {
-    this.isDialog1Open = true;
+    this.dialog1Visible = true;
   }
 
   closeDialog1() {
-    this.isDialog1Open = false;
+    this.dialog1Visible = false;
   }
 
-  saveDialog1() {
-    // Save logic for Dialog 1
-    this.closeDialog1();
-  }
-
-  cancelDialog1() {
-    // Cancel logic for Dialog 1
-    this.closeDialog1();
-  }
+  // Dialog 2
+  dialog2Visible = false;
+  dialog2Title = 'Simple dialog with actions';
 
   openDialog2() {
-    this.isDialog2Open = true;
+    this.dialog2Visible = true;
   }
 
   closeDialog2() {
-    this.isDialog2Open = false;
+    this.dialog2Visible = false;
   }
 
-  submitDialog2() {
-    // Submit logic for Dialog 2
-    this.closeDialog2();
+  // Dialog 3
+  dialog3Visible = false;
+  dialog3Title = 'Dialog with form inputs';
+
+  openDialog3() {
+    this.dialog3Visible = true;
+  }
+
+  closeDialog3() {
+    this.dialog3Visible = false;
+  }
+
+  // Dialog 4
+  dialog4Visible = false;
+  dialog4Title = 'Dialog with third-party elements';
+
+  openDialog4() {
+    this.dialog4Visible = true;
+  }
+
+  closeDialog4() {
+    this.dialog4Visible = false;
+  }
+
+  submmitDialog4() {
+    // Do something with the form
+
+    // Then, close the dialog
+    this.closeDialog4();
+  }
+
+  // Dialog 5
+  dialog5Visible = false;
+  dialog5Title = 'Small dialog';
+
+  openDialog5() {
+    this.dialog5Visible = true;
+  }
+
+  closeDialog5() {
+    this.dialog5Visible = false;
+  }
+
+  // Dialog 6
+  dialog6Visible = false;
+  dialog6Title = 'Medium dialog';
+
+  openDialog6() {
+    this.dialog6Visible = true;
+  }
+
+  closeDialog6() {
+    this.dialog6Visible = false;
+  }
+
+  // Dialog 5
+  dialog7Visible = false;
+  dialog7Title = 'Large dialog';
+
+  openDialog7() {
+    this.dialog7Visible = true;
+  }
+
+  closeDialog7() {
+    this.dialog7Visible = false;
   }
 }
--- src/app/nordea-dialog/nordea-dialog.component.ts
+++ src/app/nordea-dialog/nordea-dialog.component.ts
@@ -1,19 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
+type DialogSize = 'small' | 'medium' | 'large';
 
 @Component({
   selector: 'nordea-dialog',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NzIconModule],
   templateUrl: './nordea-dialog.component.html',
   styleUrl: './nordea-dialog.component.scss',
 })
 export class NordeaDialogComponent {
-  @Input() isOpen: boolean = false;
-  @Input() title: string = 'Dialog Title';
-  @Output() close = new EventEmitter<void>();
+  @Input() opened: boolean = false;
+  @Input() title: string = '';
+  @Input() size: DialogSize = 'medium';
 
-  closeDialog() {
-    this.close.emit();
+  @Output('dialog-close') closeEvent = new EventEmitter<void>();
+
+  get dialogClasses() {
+    return {
+      [`dialog-${this.size}`]: true,
+    };
+  }
+
+  public close() {
+    this.closeEvent.emit();
   }
 }
